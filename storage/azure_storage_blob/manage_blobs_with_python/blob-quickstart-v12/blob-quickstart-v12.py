aa86b0c17e70cc134c25c6937ce0205c1fb3dce8
Richard Vlas <vlasrichard@gmail.com>
2022-02-26 12:04:55 +0100

    added create container

--- storage/azure_storage_blob/manage_blobs_with_python/blob-quickstart-v12/blob-quickstart-v12.py
+++ storage/azure_storage_blob/manage_blobs_with_python/blob-quickstart-v12/blob-quickstart-v12.py
@@ -4,7 +4,26 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, _
 try:
     print("Azure Blob Storage v" + __version__ + " - Python quickstart sample")
 
-    # Quick start code goes here
+    # Retrieve the connection string for use with the application. The storage
+    # connection string is stored in an environment variable on the machine
+    # running the application called AZURE_STORAGE_CONNECTION_STRING. If the environment variable is
+    # created after the application is launched in a console or with Visual Studio,
+    # the shell or application needs to be closed and reloaded to take the
+    # environment variable into account.
+    connection_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+
+    # Create the BlobServiceClient object which will be used to create a container client
+    blob_service_client = BlobServiceClient.from_connection_string(connection_str)
+
+    # Create a unique name for the container
+    container_name = str(uuid.uuid4())
+
+    # Create the container
+    container_client = blob_service_client.create_container(container_name)
+
+    print(container_name)
+    print(container_client)
+    
 
 except Exception as ex:
     print("Exception:")
