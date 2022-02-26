51ae59274b0d0bdfc3f819b108450ca87c18ed4a
Richard Vlas <vlasrichard@gmail.com>
2022-02-26 13:23:08 +0100

    added blob code

--- storage/azure_storage_blob/manage_blobs_with_python/blob-quickstart-v12/blob-quickstart-v12.py
+++ storage/azure_storage_blob/manage_blobs_with_python/blob-quickstart-v12/blob-quickstart-v12.py
@@ -21,8 +21,34 @@ try:
     # Create the container
     container_client = blob_service_client.create_container(container_name)
 
-    print(container_name)
-    print(container_client)
+    # Create a local directory to hold blob data
+    local_path = "./data"
+    os.mkdir(local_path)
+
+    # Create a file in the local data directory to upload and download
+    local_file_name = str(uuid.uuid4()) + ".txt"
+    print(local_file_name)
+    upload_file_path = os.path.join(local_path, local_file_name)
+
+    # Write text to the file
+    file = open(upload_file_path, 'w')
+    file.write("Hello, World!")
+    file.close()
+
+    # Create a blob client using the local file name as the name for the blob
+    blob_client = blob_service_client.get_blob_client(container=container_name,
+                                                      blob=local_file_name)
+
+    print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
+
+    # Upload the created file
+    with open(upload_file_path, "rb") as data:
+        blob_client.upload_blob(data)
+                                                 
+
+
+
+
     
 
 except Exception as ex:
